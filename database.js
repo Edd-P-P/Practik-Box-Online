76003c5f3113b759a01136c9baf811dc8ddce419
AlejandroCis12 <alejandro240915@gmail.com>
2025-10-03 14:20:05 -0600

    productos y accesorios automaticos utilizando js

---
+++ database.js
@@ -0,0 +1,141 @@
+const productos = {
+    1: {
+        nombre: 'Closets y vestidores',
+        descripcion: 'Diseños optimizados para maximizar el espacio de almacenamiento.',
+        imagenes: [ 
+           "Imagenes/Productos/Closets y Vestidores.jpg"
+        ]
+    },
+
+    2: {
+        nombre: 'Muebles de cocina',
+        descripcion: 'Cocinas funcionales y estéticas adaptadas a tus necesidades.',
+        imagenes: [
+            "Imagenes/Productos/Muebles de cocina.jpg"
+        ]
+    },
+
+    3: {
+        nombre: 'Muebles de baño',
+        descripcion: 'Muebles resistentes a la humedad con diseños modernos.',
+        imagenes: [
+            "Imagenes/Productos/Mueble de baño.jpg"
+        ]
+    },
+    
+    4: {
+        nombre: 'Repisas',
+        descripcion: 'Soluciones creativas para organizar y exhibir tus libros y objetos.',
+        imagenes: [
+            "Imagenes/Productos/Repisa.jpg"
+        ]
+    },
+    
+    5: {
+        nombre: 'Armarios',
+        descripcion: 'Ideales para organizar ropa, calzado y accesorios con estilo y orden.',
+        imagenes: [
+            "Imagenes/Productos/Armarios Altos.jpeg"
+        ]
+    },
+    
+    6: {
+        nombre: 'Muebles de oficina',
+        descripcion: 'Escritorios amplios, gabinetes con cajones inteligentes y estanterías modulares que optimizan el espacio sin sacrificar estilo.',
+        imagenes: [
+            "Imagenes/Productos/Escritorio.jpg"
+        ]
+    },
+    
+    7: {
+        nombre: 'Pergola',
+        descripcion: 'Estructura resistente y estética, fabricada en madera tratada para exteriores. Ideal para sombrear terrazas, jardines o patios.',
+        imagenes: [
+            "Imagenes/Productos/pergola.jpg"
+        ]
+    },
+    
+    8: {
+        nombre: 'Accesorios.',
+        descripcion: 'Productos únicos de venta directa para decorar y organizar tu espacio. Cajas de té, tablas de queso y más.',
+        imagenes: [
+            "Imagenes/Index/Accesorios.png"
+        ]
+    }
+}
+
+const accesorios = {
+    1: {
+        nombre: 'Cajas de Té',
+        descripcion: 'Hermosa caja de madera con divisores internos para organizar tus tés favoritos. Acabado natural que realza la veta de la madera.',
+        imagenes: [ 
+            "Imagenes/Index/Accesorios.png"
+        ]
+    },
+
+    2: {
+        nombre: 'Tablas para queso',
+        descripcion: 'Descubre la elegancia natural de nuestras tablas: una fusión perfecta de diseño, funcionalidad y sabor.',
+        imagenes: [ 
+            "Imagenes/Productos/Tablas para queso.png"
+        ]
+    },
+
+    3: {
+        nombre: 'Centro de mesa rústico',
+        descripcion: 'Hermoso lazy susan. Ideal para dar un toque natural a tu living o comedor.',
+        imagenes: [ 
+            "Imagenes/Productos/lazy susan.png"
+        ]
+    },
+
+    4: {
+        nombre: 'Banquitos',
+        descripcion: 'Banquitos de madera que fusionan elegancia atemporal con funcionalidad. Diseños cuidadosamente elaborados para aportar calidez y estilo a cualquier espacio.',
+        imagenes: [ 
+            "Imagenes/Index/Accesorios.png"
+            
+        ]
+    },   
+
+    5: {
+        nombre: 'Botes de basura',
+        descripcion: 'Botes de basura en madera con tapa hermética y diseño discreto. Ideal para integrarse en cocinas o baños con estilo natural y funcional.',
+        imagenes: [ 
+            "Imagenes/Productos/bote.jpg" 
+        ]
+    },   
+
+    6: {
+        nombre: 'Macetas',
+        descripcion: 'Macetas de madera con forro interior resistente a la humedad. Perfectas para plantas de interior y para aportar calidez a cualquier rincón.',
+        imagenes: [ 
+            "Imagenes/Productos/macetas.jpg"
+
+        ]
+    }, 
+
+    7: {
+        nombre: 'Porta huevos',
+        descripcion: 'Porta huevos artesanal en madera, con compartimentos individuales para mantener el orden y frescura en tu cocina.',
+        imagenes: [ 
+            "Imagenes/Productos/porta huevo.jpg" 
+        ]
+    }, 
+
+    8: {
+        nombre: 'Tablas para baguette',
+        descripcion: 'Tablas alargadas en madera natural, ideales para servir baguettes, pan artesanal o quesos. Resistente, ligera y con acabado suave al tacto.',
+        imagenes: [ 
+            "Imagenes/Productos/Tablas para baguette.jpg" 
+        ]
+    },
+
+    9: {
+        nombre: 'Soporte de velas',
+        descripcion: 'Soporte de velas en madera maciza, con diseño minimalista que realza la luz cálida de cualquier vela. Ideal para ambientes relajantes o decoración de mesa.',
+        imagenes: [ 
+            "Imagenes/Productos/soporte de velas.jpg"
+        ]
+    }
+}
